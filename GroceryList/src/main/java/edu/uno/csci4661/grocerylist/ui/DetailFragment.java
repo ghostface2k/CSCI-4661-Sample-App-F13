fe40ccab11e7e8e74077144d433db2d6f7d5b52b
Daniel Ward <dwa012@gmail.com>
2013-09-12 16:42:28 -0500

    added the detail fragment and some starter code

---
+++ GroceryList/src/main/java/edu/uno/csci4661/grocerylist/ui/DetailFragment.java
@@ -0,0 +1,32 @@
+package edu.uno.csci4661.grocerylist.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by danielward on 9/12/13.
+ */
+public class DetailFragment extends Fragment {
+    public static final String ITEM_ID = "item_id";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = null;
+        // TODO add the some code to inflate from a layout
+
+        if (this.getArguments().size() == 0) {
+            throw new IllegalArgumentException("Needs to have an item id as an argument");
+        }
+
+        // this will throw an exeption if no id was given
+        int id = this.getArguments().getInt(ITEM_ID);
+
+
+        // TODO get the item and then show the relevant info
+
+        return view;
+    }
+}
